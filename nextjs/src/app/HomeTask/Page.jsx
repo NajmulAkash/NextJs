929248c97bb600f7d06d9d6c61d506895a093873
Najmul Akash <najmulbalghari23@gmail.com>
2026-02-17 05:19:27 -0800

    navbar

---
+++ nextjs/src/app/HomeTask/Page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+function Page() {
+  return (
+    <>
+    <div className='w-full h-screen bg-red-400'>nnn</div>
+    </>
+  )
+}
+
+export default Page
--- nextjs/src/app/layout.js
+++ nextjs/src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Navbar from "./NPA/Navbar";
 import "./globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+          <Navbar/>
         {children}
       </body>
     </html>
